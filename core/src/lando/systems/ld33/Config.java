1e5c7f9bd3376c1da62769d759f626b20190b69a
Doug Graham <dsgraham@gmail.com>
2015-08-22 10:58:16 -0500

    Oops

---
+++ core/src/lando/systems/ld33/Config.java
@@ -0,0 +1,12 @@
+package lando.systems.ld33;
+
+/**
+ * Created by dsgraham on 8/22/15.
+ */
+public class Config {
+
+    public static String title = "Goomba Simulator";
+    public static int width = 640;
+    public static int height = 480;
+
+}
