074eb9fd8d226b18f24ef8d10b07529eb0c9d258
Doug Graham <dsgraham@gmail.com>
2015-08-22 10:46:20 -0500

    Set up some config stuff

--- desktop/src/lando/systems/ld33/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld33/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package lando.systems.ld33.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld33.Config;
 import lando.systems.ld33.LudumDare33;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = Config.title;
+        config.width = Config.width;
+        config.height = Config.height;
 		new LwjglApplication(new LudumDare33(), config);
 	}
 }
--- html/src/lando/systems/ld33/client/HtmlLauncher.java
+++ html/src/lando/systems/ld33/client/HtmlLauncher.java
@@ -3,13 +3,14 @@ package lando.systems.ld33.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import lando.systems.ld33.Config;
 import lando.systems.ld33.LudumDare33;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Config.width, Config.height);
         }
 
         @Override
